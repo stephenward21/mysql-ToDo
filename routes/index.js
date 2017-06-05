4bcf89d902a7dca83122f2d7bee9a68beb08ba65
Stephen Ward <stephenward@Stephens-MacBook-Pro.local>
2017-06-05 16:10:01 -0400

    express mysql connect, add/delete events, style page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
+var config = require('../config/config');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: config.host,
+	user: config.userName,
+	password: config.passWord,
+	database: config.database
+
+});
+
+console.log(config);
+
+connection.connect();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var message = req.query.msg;
+	if (message == "added"){
+		message = "your task was added!";
+	}
+	var selectQuery = "SELECT * FROM tasks ORDER BY taskDate";
+	connection.query(selectQuery, (error, results)=>{
+		res.render('index', {
+			message: message,
+			taskArray: results
+
+		});
+	});
+
+  // res.render('index', { message: message });
+
+});
+
+router.post('/addItem',(req,res)=>{
+	// res.json(req.body);
+	var newTask = req.body.newTask;
+	var dueDate = req.body.newTaskDate;
+	// We know what they submitted from the form. It comes to this route inside req.body.NAMEOFFIELD. 
+	// Now we need to insert the info into mySQL.
+	var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES ('"+newTask+"','"+dueDate+"')"
+	// res.send(insertQuery);
+	connection.query(insertQuery, (error,results)=>{
+		if(error) throw error;
+		res.redirect('/?msg=added');
+	});
+});
+
+router.get('/delete/:id', (req,res)=>{
+	var idToDelete = req.params.id;
+	var deleteQuery = "DELETE FROM tasks WHERE id = " + idToDelete;
+	connection.query(deleteQuery, (error,results)=>{
+		res.redirect('/?msg=deleted');
+	});
+	
 });
 
 module.exports = router;
